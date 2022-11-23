86d2dbb4dd81f410cd7da561598f6604b445d1e4
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-11-23 10:51:17 +0100

    stack update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-    const handleMessage = ( e ) => {
-        console.log( JSON.parse( e.data ) );
-    };
+	const handleMessage = (e) => {
+		console.log(JSON.parse(e.data));
+	};
 
-    useEffect( () => {
-        const url = new URL( 'http://localhost:1234/.well-known/mercure' );
-        url.searchParams.append('topic','lien de mon topic privé');
-        const eventSource = new EventSource(url, {withCredentials: true});
-        eventSource.onmessage = handleMessage;
-        return () => {
-            eventSource.close();
-        }
-    }, []);
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+	useEffect(() => {
+		const url = new URL("http://localhost:1234/.well-known/mercure");
+		url.searchParams.append("topic", "lien de mon topic privé");
+		const eventSource = new EventSource(url, { withCredentials: true });
+		eventSource.onmessage = handleMessage;
+		return () => {
+			eventSource.close();
+		};
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
