7aa03c1abc13420396f54a05ebe4852f4ed66b26
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-11-23 09:54:55 +0100

    first push

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-Pour lancer le projet faut
+# Mercure
+
+Une rapide démo de ping privé avec Mercure https://mercure.rocks/
+
+Pour lancer le projet :
 ```shell
-cd websocket
-composer install
-cd ..
 docker-compose up -d
 cd frontend
-npm install
 npm run start
 ```
+
+Pensez à récupérer les dépendances Yarn, Composer et à
+charger les fixtures dans le container Symfony
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import NeedAuth from "./Auth/NeedAuth";
+import UserList from "./Component/UserList";
+import Login from "./Auth/Login";
+import UserProvider from "./Context/UserContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <UserProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={
+                        <NeedAuth>
+                            <UserList/>
+                        </NeedAuth>
+                    }/>
+                    <Route path='/login' element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
+        </UserProvider>
+    );
 }
 
 export default App;
